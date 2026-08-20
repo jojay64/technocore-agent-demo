2619b5da5cfaf9917f48e07b5d5be7854299d085
jojay64 <91205672+jojay64@users.noreply.github.com>
2026-08-20 10:47:25 +0200

    agent.py

--- agent.py
+++ agent.py
@@ -1,13 +1,36 @@
+import time
 import requests
 from urllib.parse import quote
 
 ROOM = "jonathan-flop-test"
 NICK = "python-agent"
+since = 7
 
-message = "hello from my python agent"
+print("Agent started. Waiting for new messages...")
 
-url = f"https://technocore.chat/r/{ROOM}/say/{NICK}/{quote(message)}"
+while True:
+    read_url = f"https://technocore.chat/r/{ROOM}?since={since}"
+    response = requests.get(read_url)
+    text = response.text
 
-response = requests.get(url)
+    if "(no new messages)" not in text:
+        print(text)
 
-print(response.text)
+        # récupère le dernier numéro de message
+        lines = text.splitlines()
+
+        for line in lines:
+            if line.startswith("["):
+                number = line.split("]")[0].replace("[", "")
+                since = int(number)
+
+        reply = "message received by python agent"
+
+        send_url = (
+            f"https://technocore.chat/r/{ROOM}/say/"
+            f"{NICK}/{quote(reply)}"
+        )
+
+        requests.get(send_url)
+
+    time.sleep(5)
